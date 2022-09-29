a9158a720c698145b8e246c344babf71cc89fbfe
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-09-29 15:40:55 +0100

    sep 29

--- sandbox-backup/src/App.js
+++ sandbox-backup/src/App.js
@@ -1,130 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-// import Filter from './components/Filter';
+import { useState } from "react";
 
 const App = () => {
-  const [countries, setCountries] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [allCountryData, setAllCountryData] = useState([]);
-  // const [filteredCountries, setFilteredCountries] = useState([])
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  useEffect(() => {
-    axios.get('https://restcountries.com/v3.1/all').then((response) => {
-      const names = response.data.map((i) => i.name.common);
-      setCountries(names);
-      setAllCountryData(response.data);
-      // setAllCountryData(response.data.slice(0, 10));
-    });
-  }, []);
+  const handleGood = () => setGood(good + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
 
-  useEffect(() => {
-    axios
-      .get(
-        'api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=8e64341d2040d3b33c77344eb5c33395'
-      )
-      .then((response) => {console.log(response)});
-  }, []);
+  const Heading = ({ text }) => <h1>{text}</h1>;
 
-  const filteredCountries = countries.filter((item) => {
-    if (searchTerm === '') {
-      return item;
-    } else if (item.toLowerCase().includes(searchTerm.toLowerCase())) {
-      return item;
+  const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>{text}</button>
+  );
+
+  const StatisticLine = ({ text, value, symbol }) => (
+    <>
+      <tr>
+        <td>
+          {text}
+        </td>
+        <td>
+          {value} {symbol}
+        </td>
+      </tr>
+    </>
+  );
+
+  const Statistics = () => {
+    if ((good || neutral || bad) === 0) {
+      return <div>No feedback given</div>;
+    } else {
+      return (
+        <>
+          <table>
+            <tbody>
+              <StatisticLine text="good" value={good} />
+              <StatisticLine text="neutral" value={neutral} />
+              <StatisticLine text="bad" value={bad} />
+              <StatisticLine text="all" value={good + neutral + bad} />
+              <StatisticLine
+                text="average"
+                value={(good - bad) / (good + neutral + bad)}
+              />
+              <StatisticLine
+                text="positive"
+                value={good / (good + neutral + bad)}
+                symbol="%"
+              />
+            </tbody>
+          </table>
+        </>
+      );
     }
-  });
-
-  // console.log(filteredCountries);
-  // console.log(countries);
-  // console.log(allCountryData);
-
-  const CountryView = ({ allCountryData, filteredCountries }) => {
-    return (
-      <>
-        <div>
-          <h1>
-            {
-              allCountryData[countries.indexOf(filteredCountries.join())].name
-                .common
-            }
-          </h1>
-          <p>
-            capital{' '}
-            {
-              allCountryData[countries.indexOf(filteredCountries.join())]
-                .capital
-            }
-          </p>
-          <p>
-            area{' '}
-            {allCountryData[countries.indexOf(filteredCountries.join())].area}
-          </p>
-          <p>
-            <strong>languages:</strong>{' '}
-            {Object.values(
-              allCountryData[countries.indexOf(filteredCountries.join())]
-                .languages
-            ).map((item) => (
-              <li key={item}>{item}</li>
-            ))}
-          </p>
-          <p>
-            <img
-              style={{ width: 100 }}
-              src={
-                allCountryData[countries.indexOf(filteredCountries.join())]
-                  .flags.svg
-              }
-              alt='flag'
-            />
-          </p>
-          <p>
-            <h2>
-              Weather in{' '}
-              {
-                allCountryData[countries.indexOf(filteredCountries.join())]
-                  .capital
-              }
-            </h2>
-          </p>
-        </div>
-      </>
-    );
   };
 
   return (
-    <>
-      <div>
-        <label>
-          <span>find countries</span>
-          <input
-            type='text'
-            placeholder='Search here...'
-            onChange={(event) => {
-              setSearchTerm(event.target.value);
-            }}
-          />
-        </label>
-        <div>
-          {filteredCountries.length === 1 ? (
-            <CountryView
-              filteredCountries={filteredCountries}
-              allCountryData={allCountryData}
-            />
-          ) : filteredCountries.length < 10 ? (
-            filteredCountries.map((item) => (
-              <div key={item}>
-                <span>
-                  {item}{' '}
-                  <button onClick={() => setSearchTerm(item)}>show</button>
-                </span>
-              </div>
-            ))
-          ) : (
-            'Too many matches, specify another filter'
-          )}
-        </div>
-      </div>
-    </>
+    <div>
+      <Heading text="give feedback" />
+      <Button onClick={handleGood} text="good" />
+      <Button onClick={handleNeutral} text="neutral" />
+      <Button onClick={handleBad} text="bad" />
+
+      <Heading text="statistics" />
+
+      <Statistics />
+    </div>
   );
 };
 
